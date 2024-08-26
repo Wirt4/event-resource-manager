860fd36dc6322d1c36c25e523688ecbd666630c5
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-08-26 15:55:22 -0700

    moved stray globals in scope

--- code/foodfinder-application/middleware/db-connect.ts
+++ code/foodfinder-application/middleware/db-connect.ts
@@ -1,15 +1,17 @@
 import mongoose, { ConnectOptions } from "mongoose"
 
-const MONGO_URI = process.env.MONGO_URI || ""
-if (!MONGO_URI.length) {
-    throw new Error("Please define the MONGO_URI environment variable(.env.local)")
-}
 
-let cached = global.mongoose
-if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null }
-}
 async function dbConnect(): Promise<any> {
+
+    const MONGO_URI = process.env.MONGO_URI || ""
+    if (!MONGO_URI.length) {
+        throw new Error("Please define the MONGO_URI environment variable(.env.local)")
+    }
+
+    let cached = global.mongoose
+    if (!cached) {
+        cached = global.mongoose = { conn: null, promise: null }
+    }
     if (cached.conn) {
         return cached.conn
     }
