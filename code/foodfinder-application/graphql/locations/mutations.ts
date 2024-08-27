2814f5dd2cea6ba9e91df8cf33f33524db4d7128
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-08-27 12:20:07 -0700

    move magic strings into an actions enum

--- code/foodfinder-application/graphql/locations/mutations.ts
+++ code/foodfinder-application/graphql/locations/mutations.ts
@@ -1,13 +1,19 @@
 import { updateWishList } from "@/mongoose/locations/services"
 import { UpdateWishlistInterface } from "@/mongoose/locations/custom"
 
+enum actions {
+    REMOVE = "remove",
+    ADD = "add"
+
+}
+
 export const locationMutations = {
     removeWishlist: async function (_: any,
         param: UpdateWishlistInterface) {
-        return updateWishList(param.location_id, param.user_id, "remove")
+        return updateWishList(param.location_id, param.user_id, actions.REMOVE)
     },
 
     addWishlist: async function (_: any, param: UpdateWishlistInterface) {
-        return updateWishList(param.location_id, param.user_id, "add")
+        return updateWishList(param.location_id, param.user_id, actions.ADD)
     }
 }
