6a5a03550be68401c9d1c7f64950152d5b3aa0af
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-08-26 17:07:36 -0700

    rounded out services

--- code/foodfinder-application/mongoose/locations/services.ts
+++ code/foodfinder-application/mongoose/locations/services.ts
@@ -1,6 +1,7 @@
 import { LocationType } from "mongoose/locations/schema"
 import { FilterLocationType, FilterWishlistType } from "mongoose/locations/custom"
 import Locations from "mongoose/locations/model"
+import { QueryOptions } from "mongoose"
 export async function findAllLocations(): Promise<LocationType[] | []> {
     const filter = {}
     return findLocations(filter)
@@ -17,10 +18,39 @@ async function findLocations(filter: FilterLocationType | FilterWishlistType | {
 }
 
 export async function findLocationById(location_id: string[]): Promise<LocationType[] | []> {
-    const filter = { location_id: location_id }
+    const filter = { location_id }
     return findLocations(filter)
 }
 
-//TODO: onuserwishlist
+export async function onUserWishList(user_id: string): Promise<LocationType[] | []> {
+    const filter: FilterWishlistType = {
+        on_wishlist: {
+            $in: [user_id]
+        }
+    }
+    return findLocations(filter)
+}
 
-//TODO: update wishlist
+export async function updateWishList(location_id: string, user_id: string, action: string): Promise<LocationType | {} | null> {
+    const filter = { location_id }
+    const options: QueryOptions = { upsert: true, returnDocument: "after" }
+    const user_filter = { on_wishlist: user_id }
+    let update = {}
+
+    switch (action) {
+        case "add":
+            update = { $push: user_filter }
+            break
+        case "remove":
+            update = { $pull: user_filter }
+            break
+        default:
+            throw new Error("action is neither ADD nor REMOVE")
+    }
+    try {
+        return Locations.findOneAndUpdate(filter, update, options)
+    } catch (err) {
+        console.error({ err })
+    }
+    return {}
+}
