18d7dd1a60fe3482e3987742b8d169b12a383642
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-08-27 11:46:00 -0700

    remove magic numbers

--- code/foodfinder-application/graphql/locations/custom.gql.ts
+++ code/foodfinder-application/graphql/locations/custom.gql.ts
@@ -1,9 +1,11 @@
 //don't like the magic numbers below
 
 // provides the gql schema for the custom types
+const one_minute = 60
+const twenty_four_hours = 86400
 export default `
     directive @cacheControl(maxAge: Int) on FIELD_DEFINITION | OBJECT
-    type Location @cacheControl(maxAge: 86400) {
+    type Location @cacheControl(maxAge: ${twenty_four_hours}) {
         address: String
         street: String
         zipcode: String
@@ -11,7 +13,7 @@ export default `
         cuisine: String
         grade: String
         name: String
-        on_wishlist: [String] @cacheControl(maxAge: 60)
+        on_wishlist: [String] @cacheControl(maxAge: ${one_minute})
         location_id: String
     }
 `
