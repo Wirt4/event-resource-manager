e97f4fb6e168103ba639fa1bef5449da07485211
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-08-28 18:15:28 -0700

    implemented first pass of landing page

--- code/foodfinder-application/pages/index.tsx
+++ code/foodfinder-application/pages/index.tsx
@@ -1,7 +1,42 @@
-import type { NextPage } from "next"
+import Head from "next/head"
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from "next"
 
-const Home: NextPage = function () {
-  return (<div>Greetings Program</div>)
+import LocationsList from "@/components/locations-list"
+import dbConnect from "@/middleware/db-connect"
+import { findAllLocations } from "@/mongoose/locations/services"
+import { LocationType } from "@/mongoose/locations/schema"
+
+const Home: NextPage = (props: InferGetStaticPropsType<getStaticProps>) => {
+  const locations: LocationType[] = JSON.parse(props.data?.locations)
+  let title = `The Food Finder - Home`
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content="The Food Finder - Home" />
+
+        <h1>Welcome to the Food Finder!</h1>
+        <LocationsList locations={locations} />
+      </Head>
+    </div>
+  )
+}
+
+//Why is `getStaicProps` exported here
+//plus, confusing naming convention
+export const getStaticProps: GetStaticProps = async () => {
+  let locations: LocationType[] | []
+  try {
+    await dbConnect()
+    locations = await findAllLocations()
+  } catch (err: any) {
+    return { notFound: true }
+  }
+  return {
+    props: {
+      data: { locations: JSON.stringify(locations) }
+    }
+  }
 }
 
 export default Home
