a3363f0e3c5f1ece9a4636230e4918b7848eda5f
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-08-30 18:30:08 -0700

    implement some oauth functionality

---
+++ code/foodfinder-application/custom.d.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+declare global {
+    var mongoose: mongoose
+}
+
+declare module "next-auth" {
+    interface Session {
+        user: {
+            fdlst_private_userId: string
+        } & DefaultSession["user"]
+    }
+}
---
+++ code/foodfinder-application/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,37 @@
+import { createHash } from "crypto"
+import { NextApiRequest, NextApiResponse } from "next"
+import NextAuth from "next-auth/next"
+import GithubProvider from "next-auth/providers/github"
+
+const createUserId = (base: string): string => {
+    return createHash("sha256").update(base).digest("hex")
+}
+
+//Is this inconsistent form here?
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+    return await NextAuth(req, res, {
+        providers: [
+            GithubProvider({
+                clientId: process.env?.GITHUB_CLIENT_ID || "",
+                clientSecret: process.env?.GITHUB_CLIENT_SECRET || ""
+            })
+        ],
+        //callbacks seem repetitive: can we abstract them?
+        callbacks: {
+            async jwt({ token }) {
+                if (token?.email && !token.fdlst_private_usserId) {
+                    token.fdlst_private_usserId = createUserId(token.email)
+                }
+                return token
+            },
+            async session({ session }) {
+                if (session?.user?.email && !session?.user?.fdlst_private_usserId) {
+                    session.user.fdlst_private_usserId = createUserId(session?.user?.email)
+                }
+                return session
+            }
+        }
+    })
+
+}
