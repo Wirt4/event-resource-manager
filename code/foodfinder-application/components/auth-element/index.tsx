6f5024fc163b9d18103e4a8e39b22ddc1ffc3903
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-09-01 11:31:38 -0700

    refactor Oath for shorter function bodies

--- code/foodfinder-application/components/auth-element/index.tsx
+++ code/foodfinder-application/components/auth-element/index.tsx
@@ -3,31 +3,51 @@ import styles from "./index.module.css"
 import { signIn, useSession, signOut } from "next-auth/react"
 import Link from "next/link"
 
+interface Session {
+    user: {
+        name: string
+        fdlst_private_userId: string
+    }
+}
+
+const greeting = (session: Session): JSX.Element => {
+    return (<span className={styles.name}>
+        Hello <strong>{session?.user.name}</strong>
+    </span>)
+}
+
+const SignInButton = (): JSX.Element => {
+    return (<>
+        <Button variant="blue" clickHandler={() => signIn()}>
+            Sign In
+        </Button>
+    </>)
+}
+
+const WishListButton = (session: Session): JSX.Element => {
+    return (<Button variant="outline">
+        <Link href={`/list/${session?.user.fdlst_private_userId}`}>
+            Your wish list
+        </Link>
+    </Button>)
+}
+
+const signOutButton = (): JSX.Element => {
+    return (<Button variant="blue" clickHandler={() => signOut()}>
+        Sign Out
+    </Button>)
+}
+
 const AuthElement = (): JSX.Element => {
     const { data: session, status } = useSession()
     return (<>
-        {status === "authenticated" && (<span className={styles.name}>
-            Hello <strong>{session?.user.name}</strong>
-        </span>)}
+        {status === "authenticated" && (greeting(session))}
         <nav className={styles.root}>
-            {status === "unauthenticated" && (
-                <>
-                    <Button variant="blue" clickHandler={() => signIn()}>
-                        Sign In
-                    </Button>
-                </>
-            )}
+            {status === "unauthenticated" && (SignInButton())}
             {status === "authenticated" && (
                 <>
-                    <Button variant="outline">
-                        <Link href={`/list/${session?.user.fdlst_private_userId}`}>
-                            Your wish list
-                        </Link>
-                    </Button>
-
-                    <Button variant="blue" clickHandler={() => signOut()}>
-                        Sign Out
-                    </Button>
+                    {WishListButton(session)}
+                    {signOutButton()}
                 </>
             )}
         </nav>
