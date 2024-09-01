c60bb7207717a19ab1903a205a3b763d6d76e7b2
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-09-01 11:22:41 -0700

    detaangle nextauth a little

--- code/foodfinder-application/pages/api/auth/[...nextauth].ts
+++ code/foodfinder-application/pages/api/auth/[...nextauth].ts
@@ -3,37 +3,40 @@ import { NextApiRequest, NextApiResponse } from "next"
 import NextAuth from "next-auth/next"
 import GithubProvider from "next-auth/providers/github"
 
+const GITHUB_CLIENT_ID = process.env?.GITHUB_CLIENT_ID || ""
+const GITHUB_CLIENT_SECRET = process.env?.GITHUB_CLIENT_SECRET || ""
+
 const createUserId = (base: string): string => {
     return createHash("sha256").update(base).digest("hex")
 }
+/**
+ * relies on JS's pass-by-ref for non-primitives
+ * @param obj 
+ * void
+ */
+const userIdFromEmail = (obj: any) => {
+    if (obj && obj?.email && !obj.fdlst_private_userId) {
+        obj.fdlst_private_userId = createUserId(obj.email)
+    }
+}
 
-//Is this inconsistent form here?
-
-async function auth(req: NextApiRequest, res: NextApiResponse) {
-    return await NextAuth(req, res, {
-        providers: [
-            GithubProvider({
-                clientId: process.env?.GITHUB_CLIENT_ID || "",
-                clientSecret: process.env?.GITHUB_CLIENT_SECRET || ""
-            })
-        ],
-        //callbacks seem repetitive: can we abstract them?
+const auth = async function (req: NextApiRequest, res: NextApiResponse) {
+    return NextAuth(req, res, {
+        providers: [GithubProvider({
+            clientId: GITHUB_CLIENT_ID,
+            clientSecret: GITHUB_CLIENT_SECRET
+        })],
         callbacks: {
             async jwt({ token }) {
-                if (token?.email && !token.fdlst_private_usserId) {
-                    token.fdlst_private_usserId = createUserId(token.email)
-                }
+                userIdFromEmail(token)
                 return token
             },
             async session({ session }) {
-                if (session?.user?.email && !session?.user?.fdlst_private_usserId) {
-                    session.user.fdlst_private_usserId = createUserId(session?.user?.email)
-                }
+                userIdFromEmail(session?.user)
                 return session
             }
         }
     })
-
 }
 
 export default auth
