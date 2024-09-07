4c6ab040b597c27b93cda6e40784528c81c0e87a
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-09-07 11:20:22 -0700

    change status to useful code
    even if it's wrapped in a gql 500, there will still be more information

--- code/foodfinder-application/middleware/auth-guards.ts
+++ code/foodfinder-application/middleware/auth-guards.ts
@@ -14,7 +14,7 @@ import {JWT} from "next-auth/jwt"
     if (!context || !context.token || !context.token.fdlst_private_userId) {
         return new GraphQLError("User is not authenticated",{
             extensions:{
-                http:{status: 500},//TODO: can I give this a less useless status?
+                http:{status: 401},
                 code: "UNAUTHENTICATED"
             }
         })
@@ -24,7 +24,7 @@ import {JWT} from "next-auth/jwt"
         console.error({param})
         return new GraphQLError("User is not authorized",{
             extensions:{
-                http:{status: 500},//TODO: can I give this a less useless status?
+                http:{status: 401},
                 code: "UNAUTHORIZED"
             }
         })
