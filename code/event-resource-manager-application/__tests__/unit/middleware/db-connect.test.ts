1b72e6003bb34105177edef97ce2da77a63003e2
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-09-15 14:28:03 -0700

    more test engineering

--- code/event-resource-manager-application/__tests__/unit/middleware/db-connect.test.ts
+++ code/event-resource-manager-application/__tests__/unit/middleware/db-connect.test.ts
@@ -1,10 +1,22 @@
 import dbConnect from "@/middleware/db-connect"
+import mongoose from "mongoose";
 
 describe('dbConnect',()=>{
     const originalEnv = process.env
+    const originalGlobal = global
+    beforeEach(()=>{
+        process.env = {
+            ...originalEnv,
+            MONGO_URI: 'non-stupid URI',
+        };
+    })
     afterEach(()=>{
         process.env = originalEnv
+        global = originalGlobal
+        jest.clearAllMocks();
+
     })
+
     test('MONGO_URI is not defined', async ()=> {
         process.env.MONGO_URI = ''
 
@@ -18,14 +30,32 @@ describe('dbConnect',()=>{
     })
 
     test('MONGO_URI is defined, does not throws error', async ()=> {
-        process.env = {
-            ...originalEnv,
-            MONGO_URI: 'non-stupid URI',
-        };
+        jest.spyOn(mongoose, 'connect').mockResolvedValue(mongoose)
         try{
             await dbConnect()
         }catch(error){
             expect(true).toBe(false)
         }
     })
+
+    test('A cached connection already exists', async ()=>{
+        global.mongoose = {conn: "valid connection"}
+        const result = await dbConnect()
+        expect(result).toEqual("valid connection")
+    })
+
+    test('A cached connection already exists, mongoose is not called', async ()=>{
+        global.mongoose = {conn: "valid connection"}
+        const spy = jest.spyOn(mongoose, 'connect').mockResolvedValue(mongoose)
+        await dbConnect()
+        expect(spy).not.toHaveBeenCalled()
+    })
+
+    test('there is no cached connection nor promise, expect mongo connect to have been called', async()=>{
+        global.mongoose = null
+        const spy = jest.spyOn(mongoose, 'connect').mockResolvedValue(mongoose)
+        await dbConnect()
+        expect(spy).toHaveBeenCalled()
+    })
+
 })
--- code/event-resource-manager-application/middleware/db-connect.ts
+++ code/event-resource-manager-application/middleware/db-connect.ts
@@ -1,6 +1,13 @@
+import mongoose from "mongoose";
 
 async function dbConnect(): Promise<any> {
        if (!process?.env?.MONGO_URI) {throw new Error('Please define the MONGO_URI environment variable (.env.local)')}
+       if (global?.mongoose?.conn){
+              return global.mongoose.conn
+       }
+
+       await mongoose.connect('foo')
+
 }
 
 export default dbConnect
