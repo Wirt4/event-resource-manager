6398d256bfec5304d1627dd0926c70ffe10cb702
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-09-15 20:32:40 -0700

    reimplemented dbConnect TDD

--- code/event-resource-manager-application/__tests__/unit/middleware/db-connect.test.ts
+++ code/event-resource-manager-application/__tests__/unit/middleware/db-connect.test.ts
@@ -1,5 +1,6 @@
 import dbConnect from "@/middleware/db-connect"
 import mongoose from "mongoose";
+import anything = jasmine.anything;
 
 describe('dbConnect',()=>{
     const originalEnv = process.env
@@ -56,6 +57,117 @@ describe('dbConnect',()=>{
         const spy = jest.spyOn(mongoose, 'connect').mockResolvedValue(mongoose)
         await dbConnect()
         expect(spy).toHaveBeenCalled()
+    })
+
+    test('there is no cached connection, but there is a promise, expect that promise to be called', async()=>{
+        const p = new Promise((resolve, reject) => {
+            resolve('valid cached connection')
+        })
+        global.mongoose = {conn: null, promise:p}
+
+        const result = await dbConnect()
+
+        expect(result).toEqual('valid cached connection')
+    })
+
+    test("promise is cached, don't call mongoose", async()=>{
+        const p = new Promise((resolve, reject) => {
+            resolve('valid cached connection')
+        })
+        global.mongoose = {conn: null, promise:p}
+        const spy = jest.spyOn(mongoose, 'connect').mockResolvedValue(mongoose)
+
+        await dbConnect()
+
+        expect(spy).not.toHaveBeenCalled()
+    })
+
+    test("no cache, mongoose must be called with mongo uri and specs", async()=>{
+        global.mongoose = null
+        process.env = {
+            ...originalEnv,
+            MONGO_URI: 'valid_mongo_uri',
+        };
+
+        const spy = jest.spyOn(mongoose, 'connect').mockResolvedValue(mongoose)
+
+        await dbConnect()
+
+        const opts = {
+            bufferCommands: false,
+            maxIdleTimeMS: 10000,
+            serverSelectionTimeoutMS: 10000,
+            socketTimeoutMS: 20000,
+        }
+
+        expect(spy).toHaveBeenCalledWith('valid_mongo_uri', opts)
+    })
+
+    test("no cache, mongoose must be called with different mongo uri", async()=>{
+        global.mongoose = null
+        process.env = {
+            ...originalEnv,
+            MONGO_URI: 'another_valid_mongo_uri',
+        };
+        const spy = jest.spyOn(mongoose, 'connect').mockResolvedValue(mongoose)
+
+        await dbConnect()
+
+        expect(spy).toHaveBeenCalledWith('another_valid_mongo_uri',  expect.anything())
+    })
+
+    test("no cache, return what mongoose gives", async()=>{
+        global.mongoose = null
+        const p: Promise<typeof import("mongoose")> = new Promise((resolve, reject) => {
+            resolve('Frankie, the valid cached connection' as unknown as typeof import("mongoose"))
+        })
+
+        jest.spyOn(mongoose, 'connect').mockResolvedValue(p)
+
+        const result = await dbConnect()
+
+        expect(result).toEqual('Frankie, the valid cached connection')
+    })
+
+    test("no cache, return what mongoose gives, different data", async()=>{
+        global.mongoose = null
+        const p: Promise<typeof import("mongoose")> = new Promise((resolve, reject) => {
+            resolve('Wesley, the valid cached connection' as unknown as typeof import("mongoose"))
+        })
+
+        jest.spyOn(mongoose, 'connect').mockResolvedValue(p)
+
+        const result = await dbConnect()
+
+        expect(result).toEqual('Wesley, the valid cached connection')
+    })
+
+    test("call method once, no cache, the promises should cache", async()=>{
+        global.mongoose = null
+        const p: Promise<typeof import("mongoose")> = new Promise((resolve, reject) => {
+            resolve('Xena, the valid cached connection' as unknown as typeof import("mongoose"))
+        })
+
+        jest.spyOn(mongoose, 'connect').mockResolvedValue(p)
+
+        await dbConnect()
+
+        expect(global.mongoose.conn).toEqual('Xena, the valid cached connection')
+    })
+
+    test("method called twice, assumes cache works and will only call mongoose once", async()=>{
+        global.mongoose = null
+        const p: Promise<typeof import("mongoose")> = new Promise((resolve, reject) => {
+            resolve('Akira, the valid cached connection' as unknown as typeof import("mongoose"))
+        })
+
+        const spy = jest.spyOn(mongoose, 'connect').mockResolvedValue(p)
+
+        await dbConnect()
+        const resultOfSecondCall = await dbConnect()
+
+        expect(resultOfSecondCall).toEqual('Akira, the valid cached connection')
+        expect(spy).toBeCalledTimes(1)
     })
 
 })
--- code/event-resource-manager-application/middleware/db-connect.ts
+++ code/event-resource-manager-application/middleware/db-connect.ts
@@ -1,13 +1,30 @@
 import mongoose from "mongoose";
 
 async function dbConnect(): Promise<any> {
-       if (!process?.env?.MONGO_URI) {throw new Error('Please define the MONGO_URI environment variable (.env.local)')}
-       if (global?.mongoose?.conn){
-              return global.mongoose.conn
+       if (!process?.env?.MONGO_URI) {
+              throw new Error('Please define the MONGO_URI environment variable (.env.local)')
        }
 
-       await mongoose.connect('foo')
+       let cached = global.mongoose
 
+       if (!cached) cached = global.mongoose = {conn: null, promise: null}
+
+       if (cached?.conn){
+              return cached.conn
+       }
+
+       if (!cached?.promise) {
+              global.mongoose.promise = mongoose.connect(process.env.MONGO_URI, {
+                     bufferCommands: false,
+                     maxIdleTimeMS: 10000,
+                     serverSelectionTimeoutMS: 10000,
+                     socketTimeoutMS: 20000,
+              })
+       }
+
+       cached.conn = await cached.promise
+       global.mongoose = cached
+       return cached.conn
 }
 
 export default dbConnect
