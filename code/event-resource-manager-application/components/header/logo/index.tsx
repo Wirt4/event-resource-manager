7fdfd229e4aadac6537db2049b59dbd355bed7b9
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-09-15 21:03:08 -0700

    upddate logo metadata and type

--- code/event-resource-manager-application/components/header/logo/index.tsx
+++ code/event-resource-manager-application/components/header/logo/index.tsx
@@ -3,12 +3,13 @@ import Link from "next/link"
 
 import logo from "/public/assets/mask.png"
 import styles from "./index.module.css"
+import {ReactElement} from "react"
 
-const Logo = function (): JSX.Element {
+const Logo = function (): ReactElement {
     return (
         <Link href="/" passHref className={styles.root}>
             <Image
-                alt="Logo: Food Finder"
+                alt="Logo: Event Resource Management"
                 src={logo}
                 sizes="300px"
                 fill
