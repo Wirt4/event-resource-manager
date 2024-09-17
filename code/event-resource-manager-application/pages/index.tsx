bccef98fb4d2f2777002f804a2f14937f9214835
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2024-09-16 18:01:46 -0700

    stray changes from regular

--- code/event-resource-manager-application/pages/index.tsx
+++ code/event-resource-manager-application/pages/index.tsx
@@ -1,26 +1,25 @@
 import Head from "next/head"
-import type { GetStaticProps, InferGetStaticPropsType, NextPage } from "next"
-
-import LocationsList from "@/components/locations-list"
 import dbConnect from "@/middleware/db-connect"
-import { findAllLocations } from "@/mongoose/locations/services"
-import { LocationType } from "@/mongoose/locations/schema"
+import EventList from "@/components/event-list"
+import {findAllEvents} from "@/mongoose/theater_events/services"
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from "next"
+import {TheaterEventType} from "@/mongoose/theater_events/schema"
 /**
  * It's a Next-sim that the function must be named "getStaticProps" and be exported
  * Page does not compile otherwise
  * @returns GetStaticProps
  */
 export const getStaticProps: GetStaticProps = async function () {
-  let locations: LocationType[] | []
+  let events: TheaterEventType[] | []
   try {
     await dbConnect()
-    locations = await findAllLocations()
+    events = await findAllEvents()
   } catch (err: any) {
     return { notFound: true }
   }
   return {
     props: {
-      data: { locations: JSON.stringify(locations) }
+      data: { events: JSON.stringify(events) }
     }
   }
 }
@@ -31,16 +30,17 @@ export const getStaticProps: GetStaticProps = async function () {
  * @returns the landing page for the food finder
  */
 const Home: NextPage = function (props: InferGetStaticPropsType<typeof getStaticProps>) {
-  const locations: LocationType[] = JSON.parse(props.data?.locations)
+  console.log({props})
+  const events: TheaterEventType[] = JSON.parse(props.data?.events)
 
   return (
     <div>
       <Head>
-        <title>The Food Finder - Home</title>
-        <meta name="description" content="The Food Finder - Home" />
+        <title>Event Resource Manager - Home</title>
+        <meta name="description" content="Event Resource Manager - Home" />
       </Head>
-      <h1>Welcome to the Food Finder!</h1>
-      <LocationsList locations={locations} />
+      <h1>Welcome to the Event Resource Manager!</h1>
+      <EventList events={events}/>
     </div>
   )
 }
